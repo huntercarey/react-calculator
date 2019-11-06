bbeacd77fd13e42cc5931ed6fe2e45b8cbf871e6
Hunter Carey <huntercarey902@gmail.com>
2019-11-06 13:41:30 -0600

    removed  App.js default contents as well as App.css //  created Calculator class component and Calculator.css // added styling in index.css and Calculator.css // added images into assets folder. placed assets folder inside of src folder

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Calculator.css'
+
+// Main Calculator component. Contains our other Calculator components.
+class Calculator extends Component {
+    render() {
+        return(
+            <div className='Calculator'>
+                <h1>Calculator</h1>
+            </div>
+        );
+    }
+}
+
+export default Calculator;
