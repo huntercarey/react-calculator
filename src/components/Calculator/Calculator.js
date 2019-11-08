3811f95a1bd3419b940faf1e505692be1b7e5cee
Hunter Carey <huntercarey902@gmail.com>
2019-11-08 10:42:25 -0600

    added in the button component plus styling // added in state for result and equation and button handler

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -5,11 +5,30 @@ import Keypad from '../Keypad/Keypad';
 
 // Main Calculator component. Contains our other Calculator components.
 class Calculator extends Component {
+
+    // Set up a constructor function to set our initial state
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            equation: '2*5',
+            result: 0
+        }
+    }
+
+    //Function called when any of the keypad buttons are pressed
+    onButtonPress = (event) => {
+        console.log('onButtonPress called');
+    }
+
+
     render() {
         return(
             <div className='Calculator'>
-                <Screen />
-                <Keypad />
+                <Screen equation={this.state.equation} result={this.state.result}/>
+
+                {/* Passing the function itself as a prop instead of running the function */}
+                <Keypad onButtonPress={this.onButtonPress}/>
             </div>
         );
     }
--- src/components/Keypad/Keypad.js
+++ src/components/Keypad/Keypad.js
@@ -1,30 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Keypad.css';
 import KeypadRow from '../KeypadRow/KeypadRow'
+import Button from '../Button/Button';
 
 class Keypad extends Component {
     render() {
-        return(
+        return (
             <div className='Keypad'>
                 {/* ROW 1 */}
                 <KeypadRow>
-                    
+                    <Button type='primary' onButtonPress={this.props.onButtonPress}>C</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='primary'>&larr;</Button>
+                    <Button onButtonPress={this.props.onButtonPress}  type='operator'>%</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='operator'>/</Button>
                 </KeypadRow>
+
                 {/* ROW 2 */}
                 <KeypadRow>
-                    
+                    <Button onButtonPress={this.props.onButtonPress} type='primary'>7</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='primary'>8</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='primary'>9</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='operator'>*</Button>
                 </KeypadRow>
+
                 {/* ROW 3 */}
                 <KeypadRow>
-                    
+                    <Button onButtonPress={this.props.onButtonPress} type='primary'>4</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='primary'>5</Button>
+                    <Button  onButtonPress={this.props.onButtonPress}type='primary'>6</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='operator'>-</Button>
                 </KeypadRow>
+
                 {/* ROW 4 */}
                 <KeypadRow>
-                    
+                    <Button onButtonPress={this.props.onButtonPress} type='primary'>1</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='primary'>2</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='primary'>3</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='operator'>+</Button>
                 </KeypadRow>
+
                 {/* ROW 5 */}
                 <KeypadRow>
-                    
+                    <Button onButtonPress={this.props.onButtonPress} type='primary'>0</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='primary'>.</Button>
+                    <Button onButtonPress={this.props.onButtonPress} type='large'>=</Button>
+
                 </KeypadRow>
             </div>
         );
