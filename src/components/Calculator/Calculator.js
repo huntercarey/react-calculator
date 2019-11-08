addd6fa263fd837a14311e3ed58d85faf8dac04f
Hunter Carey <huntercarey902@gmail.com>
2019-11-08 13:34:44 -0600

    final commit

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Calculator.css'
 import Screen from '../Screen/Screen';
 import Keypad from '../Keypad/Keypad';
@@ -11,24 +11,67 @@ class Calculator extends Component {
         super(props);
 
         this.state = {
-            equation: '2*5',
+            equation: '',
             result: 0
         }
     }
 
     //Function called when any of the keypad buttons are pressed
     onButtonPress = (event) => {
-        console.log('onButtonPress called');
+        let equation = this.state.equation;
+        const pressedButtonValue = event.target.innerHTML;
+
+        console.log(pressedButtonValue);
+
+        if (pressedButtonValue === 'C') {
+            this.clear();
+            return;
+        } else if ((pressedButtonValue >= '0' && pressedButtonValue <= '9') || pressedButtonValue === '.') {
+            /* If the user has presseda button with a value between 0 and 9 or the period, add that value to the equation */
+            equation += pressedButtonValue;
+        } else if (['+', '-', '*', '/', '%'].indexOf(pressedButtonValue) !== -1) {
+            /* If the user has pressed a button with a value of an operator, add the operator to the equation with a space on either side */
+            equation += ` ${pressedButtonValue} `;
+        } else if (pressedButtonValue === '=') {
+            /* If the user hits the enter button, calculate stuff */
+
+            /* Try catch blocks - try to run the code in the try block. If it fails and throws an error, deal with it in the catch block */
+            try {
+                const evalResult = eval(equation);
+                const result = Number.isInteger(evalResult) ? evalResult : evalResult.toFixed(2);
+                this.setState({result});
+            } catch (error) {
+                /* if an error occurs, print it to the console and alert the user */
+                console.log(error);
+                alert('an error occurred, check to make sure your equation is correct');
+            }
+
+        } else {
+            /* If the user has pressed the back arrow, remove the last character from the equation string */
+            equation = equation.trim();
+            // equation = equation.slice(0, -1);
+            equation = equation.substr(0, equation.length -1);
+        }
+
+        /* Update our state with the new version of the equation */
+        this.setState({equation});
+    }
+
+    clear() {
+        this.setState({
+            equation: '',
+            result: 0
+        });
     }
 
 
     render() {
-        return(
+        return (
             <div className='Calculator'>
-                <Screen equation={this.state.equation} result={this.state.result}/>
+                <Screen equation={this.state.equation} result={this.state.result} />
 
                 {/* Passing the function itself as a prop instead of running the function */}
-                <Keypad onButtonPress={this.onButtonPress}/>
+                <Keypad onButtonPress={this.onButtonPress} />
             </div>
         );
     }
